6bb00becf10171732b3eba42f1d2b1f9eda8b93b
Alexander Kyei <alex.kyei94@gmail.com>
2016-01-30 17:13:28 -0500

    pushing for cole

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,3 +1,12 @@
 class ExercisesController < ApplicationController
 	before_filter :authorize
+	def new
+		@exercise = Exercise.new
+	end
+
+	def create
+	render plain: params[:article].inspect
+	end
+		
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ post '/users',to:'users#create'
 get '/login',to:"sessions_controller#new"
 post '/login',to:"sessions_controller#create"
 get '/logout',to:"sessions_controller#destroy"
-
+post '/categories', to: "categories#create"
     
 	
   root 'categories#index'
