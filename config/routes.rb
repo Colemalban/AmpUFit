89fdbefd14e3edbf570284072fd9dcf1979836ed
Cole Alban <colemalban@gmail.com>
2016-01-30 03:07:31 -0500

    Create landing page and an index page for categories

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :static_pages
+  resources :categories
   root 'static_pages#index'
 
   # Example of regular route:
